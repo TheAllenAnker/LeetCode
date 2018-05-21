d04b0801fb301be6589c7250f5d3f808ee66ee55
TheAllenAnker <theallenanker@gmail.com>
2018-05-21 20:32:11 +0800

    Adding solution for StringToInteger problem.

---
+++ problems/StringToInteger.py
@@ -0,0 +1,53 @@
+#Author: Allen Anker
+#Created by Fenyr_Allen on 20/05/2018
+
+class Solution(object):
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        result = ''
+        j = 0
+        for i in range(0, len(str)):
+            if str[i] == ' ':
+                continue
+            elif str[i].isnumeric():
+                result = self.extractNum(i-1, str)
+
+                break
+            elif str[i] == '+':
+                result = self.extractNum(i, str)
+
+                break
+            elif str[i] == '-':
+                result += '-'
+                result += self.extractNum(i, str)
+
+                break
+            else:
+                break
+
+        if len(result) == 0:
+            result += '0'
+        num = int(result)
+        num = min(num, 2**31-1)
+        num = max(num, -(2**31))
+
+        return num
+
+
+    def extractNum(self, i, str):
+        result = ''
+        for j in range(i + 1, len(str)):
+            if str[j].isnumeric():
+                result += str[j]
+            else:
+                break
+
+        return result
+
+
+solution = Solution()
+num = solution.myAtoi('-91283472332')
+print(num)
---
+++ problems/ZigZagConversion.py
@@ -0,0 +1,34 @@
+#Author: Allen Anker
+#Created by Fenyr_Allen on 20/05/2018
+
+'''
+Problem description:
+
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+'''
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+
+solution = Solution()
+s = 'ABCDEFGHIJKLMN'
+print(solution.convert(s, 3))
