6ad948a587967fda9b6ab527fbf7e7219851a85c
TheAllenAnker <theallenanker@gmail.com>
2018-06-09 19:44:01 +0800

    Add solution for problem 2 Keys Keyboard.

---
+++ problems/2_keys_keyboard.py
@@ -0,0 +1,20 @@
+# Author: Allen Anker
+# Created by Allen Anker on 07/06/2018
+
+
+'''
+Initially on a notepad only one character 'A' is present. You can perform two operations on this notepad for each step:
+
+Copy All: You can copy all the characters present on the notepad (partial copy is not allowed).
+Paste: You can paste the characters which are copied last time.
+Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted. Output the minimum number of steps to get n 'A'.
+'''
+
+
+class Solution:
+    def minSteps(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        pass
