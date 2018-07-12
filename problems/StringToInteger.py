f60eaac945c1fe61e7369c58654ccf095e4dd7df
TheAllenAnker <theallenanker@gmail.com>
2018-07-12 15:12:01 +0800

    Modified solution for problem "String to Integer (atoi)".

--- problems/StringToInteger.py
+++ problems/StringToInteger.py
@@ -1,54 +1,28 @@
-#Author: Allen Anker
-#Created by Fenyr_Allen on 20/05/2018
+# Author: Allen Anker
+# Created by Fenyr_Allen on 20/05/2018
+
 
 class Solution(object):
-    def myAtoi(self, str):
+    def my_atoi(self, str):
         """
         :type str: str
         :rtype: int
         """
-        result = ''
-        j = 0
-        for i in range(0, len(str)):
-            if str[i] == ' ':
-                continue
-            elif str[i].isnumeric():
-                result = self.extractNum(i-1, str)
+        if len(str.strip()) == 0:
+            return 0
+        ls = list(str.strip())
 
-                break
-            elif str[i] == '+':
-                result = self.extractNum(i, str)
+        sign = -1 if ls[0] == '-' else 1
+        if ls[0] in ['-', '+']:
+            del ls[0]
+        ret, i = 0, 0
+        while i < len(ls) and ls[i].isdigit():
+            ret = ret * 10 + ord(ls[i]) - ord('0')
+            i += 1
 
-                break
-            elif str[i] == '-':
-                if len(str)-1 > i and str[i+1].isnumeric():
-                    result += '-'
-                    result += self.extractNum(i, str)
-
-                break
-            else:
-                break
-
-        if len(result) == 0:
-            result += '0'
-        num = int(result)
-        num = min(num, 2**31-1)
-        num = max(num, -(2**31))
-
-        return num
-
-
-    def extractNum(self, i, str):
-        result = ''
-        for j in range(i + 1, len(str)):
-            if str[j].isnumeric():
-                result += str[j]
-            else:
-                break
-
-        return result
+        return max(-2 ** 31, min(sign * ret, 2 ** 31 - 1))
 
 
 solution = Solution()
-num = solution.myAtoi('42')
+num = solution.my_atoi('42')
 print(num)
