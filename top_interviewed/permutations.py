c832c9281e2e067c22d2e99cd471dd93dc7b5afa
TheAllenAnker <theallenanker@gmail.com>
2018-08-18 23:11:26 +0800

    Add a magic solution for problem "Permutations".

---
+++ top_interviewed/permutations.py
@@ -0,0 +1,18 @@
+# Author: Allen Anker
+# Created by Allen Anker on 17/08/2018
+"""
+Given a collection of distinct integers, return all possible permutations.
+"""
+
+
+class Solution:
+    def permutations(self, nums):
+        """
+        Return all possible permutations.
+        :param nums: Input number list
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return [[n] + p
+                for i, n in enumerate(nums)
+                for p in self.permute(nums[:i] + nums[i + 1:])] or [[]]
