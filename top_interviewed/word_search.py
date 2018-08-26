cd24c711f2b56a1cef241dd9723f79dd4ab68719
TheAllenAnker <theallenanker@gmail.com>
2018-08-26 15:33:12 +0800

    Add a solution to problem "Word Search".

---
+++ top_interviewed/word_search.py
@@ -0,0 +1,39 @@
+# Author: Allen Anker
+# Created by Allen Anker on 26/08/2018
+"""
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell,
+where "adjacent" cells are those horizontally or vertically neighboring.
+The same letter cell may not be used more than once.
+"""
+
+
+class Solution:
+    def exist(self, board, word):
+        """
+        Word search.
+        :type board: List[List[str]]
+        :type word: str
+        :return:
+        """
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
+
+    def dfs(self, board, i, j, word):
+        if len(word) == 0:  # all the characters are checked
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0] != board[i][j]:
+            return False
+        tmp = board[i][j]  # first character is found, check the remaining part
+        board[i][j] = "#"  # avoid visit agian
+        # check whether can find "word" along one direction in the four direction
+        res = self.dfs(board, i + 1, j, word[1:]) or self.dfs(board, i - 1, j, word[1:]) \
+              or self.dfs(board, i, j + 1, word[1:]) or self.dfs(board, i, j - 1, word[1:])
+        board[i][j] = tmp
+        return res
