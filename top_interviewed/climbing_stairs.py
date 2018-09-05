834a05e1507170c9d091e6aea8d3929f9fa7f524
TheAllenAnker <theallenanker@gmail.com>
2018-09-05 11:45:51 +0800

    Modify the TLE solution for "Climbing Stairs".

--- top_interviewed/climbing_stairs.py
+++ top_interviewed/climbing_stairs.py
@@ -16,11 +16,20 @@ class Solution:
         :type n: int
         :rtype: int
         """
+        # if n == 1:
+        #     return 1
+        # if n == 2:
+        #     return 2
+        # return self.climb_stairs(n - 1) + self.climb_stairs(n - 2)
+        # The pattern of result distinct ways is Fibonacci sequence.
         if n == 1:
             return 1
-        if n == 2:
-            return 2
-        return self.climb_stairs(n - 1) + self.climb_stairs(n - 2)
+        a, b = 1, 2
+        for i in range(2, n):
+            tmp = b
+            b = a + b
+            a = tmp
+        return b
 
 
 solution = Solution()
