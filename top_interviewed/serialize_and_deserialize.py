fd4af1dd29378be19a9d9cde48be3b9b63588e6c
TheAllenAnker <theallenanker@gmail.com>
2018-09-27 21:19:20 +0800

    Add a solution for problem "Serialize and Deserialize Binary Tree".

---
+++ top_interviewed/serialize_and_deserialize.py
@@ -0,0 +1,65 @@
+# Author: Allen Anker
+# Created by Allen Anker on 27/09/2018
+"""
+Serialization is the process of converting a data structure or object into a sequence of bits
+so that it can be stored in a file or memory buffer, or transmitted across a network connection link
+to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary tree.
+There is no restriction on how your serialization/deserialization algorithm should work.
+You just need to ensure that a binary tree can be serialized to a string and
+this string can be deserialized to the original tree structure.
+"""
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Codec:
+
+    def serialize(self, root):
+        """
+        Encodes a tree to a single string.
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        def rserialize(root, string):
+            """ a recursive helper function for the serialize() function."""
+            # check base case
+            if root is None:
+                string += 'None,'
+            else:
+                string += str(root.val) + ','
+                string = rserialize(root.left, string)
+                string = rserialize(root.right, string)
+            return string
+
+        return rserialize(root, '')
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+
+        def rdeserialize(l):
+            """ a recursive helper function for deserialization."""
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
